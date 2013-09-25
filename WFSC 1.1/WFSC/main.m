5ccea76feb0b8a3606c6f79af4b1e326c488c5cb
Kurt <kurtlane@live.com.au>
2013-09-26 05:20:07 +1000

    WFSC v1.1

---
+++ WFSC 1.1/WFSC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WFSC
+//
+//  Created by Kurt Lane on 23/08/13.
+//  Copyright (c) 2013 kurtlane.com.au. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
